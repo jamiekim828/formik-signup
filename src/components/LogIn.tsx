c85aaa68f98f9d80afdf5ae00f64e2042b22d021
jamiekim828 <jamiekim828@gmail.com>
2023-02-08 13:23:59 +0100

    login after signup

--- src/components/LogIn.tsx
+++ src/components/LogIn.tsx
@@ -2,21 +2,12 @@ import React, { useState } from 'react';
 import { Formik, Form } from 'formik';
 import * as Yup from 'yup';
 
-import TextField from '@mui/material/TextField';
+import { TextField } from '@mui/material';
 import { Button, InputAdornment } from '@mui/material';
 import VisibilityIcon from '@mui/icons-material/Visibility';
 import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
-import LogIn from './LogIn';
 
-const SignupSchema = Yup.object().shape({
-  firstName: Yup.string()
-    .min(2, 'Too Short!')
-    .max(50, 'Too Long!')
-    .required('Please enter your first name'),
-  lastName: Yup.string()
-    .min(1, 'Too Short!')
-    .max(50, 'Too Long!')
-    .required('Please enter your last name'),
+const LogInSchema = Yup.object().shape({
   email: Yup.string().email('Invalid email').required('Email is required'),
   password: Yup.string()
     .min(6, 'Password must be at least 6 characters')
@@ -25,40 +16,42 @@ const SignupSchema = Yup.object().shape({
       /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{6,})/,
       'Password should contain uppercase letter, lowercase letter and number'
     ),
-  confirmPassword: Yup.string()
-    .oneOf([Yup.ref('password'), null], 'Password does not match')
-    .required('Password confirmation is required'),
 });
 
-type User = {
-  firstName: string;
-  lastName: string;
+type Prop = {
   email: string;
-  password: string;
-  confirmPassword: string;
 };
 
-export default function SignUp() {
-  const [passwordVisibility, setPasswordVisibility] = useState(true);
-  const [login, setLogin] = useState(false);
-  const [email, setEmail] = useState('');
+type LogInInfo = {
+  email: string;
+  password: string;
+};
 
-  const submitHandler = (user: User) => {
-    if (user) {
-      localStorage.setItem('user', JSON.stringify(user));
-      setLogin(true);
-      setEmail(user.email);
-    }
-    if (!user) {
-      setLogin(false);
+export default function LogIn({ email }: Prop) {
+  const [passwordVisibility, setPasswordVisibility] = useState(true);
+  const [userName, setUserName] = useState('');
+  const [welcome, setWelcome] = useState(false);
+  const user = JSON.parse(localStorage.getItem('user') || '');
+  console.log(user);
+
+  const handleLogIn = (loginInfo: LogInInfo) => {
+    if (loginInfo.email === user.email) {
+      if (loginInfo.password === user.password) {
+        setUserName(user.firstName);
+        setWelcome(true);
+      } else {
+        alert('wrong password');
+      }
+    } else {
+      alert('Please sign up first');
     }
   };
 
   return (
     <div>
-      {login === false && (
+      {welcome === false && (
         <div className='signup-div'>
-          <h1>Sign Up</h1>
+          <h1>Log In</h1>
           <Formik
             initialValues={{
               firstName: '',
@@ -67,35 +60,13 @@ export default function SignUp() {
               password: '',
               confirmPassword: '',
             }}
-            validationSchema={SignupSchema}
+            validationSchema={LogInSchema}
             onSubmit={(values) => {
-              submitHandler(values);
+              handleLogIn(values);
             }}
           >
             {({ errors, touched, handleChange }) => (
               <Form className='signup'>
-                <TextField
-                  id='filled-basic'
-                  label='First Name'
-                  variant='filled'
-                  name='firstName'
-                  onChange={handleChange}
-                />
-                {errors.firstName && touched.firstName ? (
-                  <p>{errors.firstName}</p>
-                ) : null}
-
-                <TextField
-                  id='filled-basic'
-                  label='Last Name'
-                  variant='filled'
-                  name='lastName'
-                  onChange={handleChange}
-                />
-                {errors.lastName && touched.lastName ? (
-                  <p>{errors.lastName}</p>
-                ) : null}
-
                 <TextField
                   id='filled-basic'
                   label='E-mail'
@@ -132,34 +103,6 @@ export default function SignUp() {
                   <p>{errors.password}</p>
                 ) : null}
 
-                <TextField
-                  id='filled-basic'
-                  label='Password Confirmation'
-                  variant='filled'
-                  name='confirmPassword'
-                  type={passwordVisibility ? 'text' : 'password'}
-                  onChange={handleChange}
-                  InputProps={{
-                    endAdornment: (
-                      <InputAdornment position='end'>
-                        {passwordVisibility ? (
-                          <VisibilityIcon
-                            onClick={() => setPasswordVisibility(false)}
-                          />
-                        ) : (
-                          <VisibilityOffIcon
-                            onClick={() => setPasswordVisibility(true)}
-                          />
-                        )}
-                      </InputAdornment>
-                    ),
-                  }}
-                />
-
-                {errors.confirmPassword && touched.confirmPassword ? (
-                  <p>{errors.confirmPassword}</p>
-                ) : null}
-
                 <div className='submit'>
                   <Button
                     type='submit'
@@ -178,7 +121,11 @@ export default function SignUp() {
           </Formik>
         </div>
       )}
-      {login === true && <LogIn email={email} />}
+      {welcome === true && (
+        <div className='signup-div'>
+          <h1>Welcome {userName}!</h1>
+        </div>
+      )}
     </div>
   );
 }
